1654e37d6fa05b8ee2236d76f625963d90357e44
sreenu-reddy <sreenu.nalamalpu@gmail.com>
2021-03-16 15:56:47 +0100

    added apache Commons StringUtils To HelloWorld Class

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(StringUtils.capitalize("HelloWorld"));
+    }
+}
